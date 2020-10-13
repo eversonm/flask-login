8598620af934930e2accb047c77fc20518a59e07
Everson Magalhaes <everson.m.cavalcante@gmail.com>
2020-10-13 15:24:16 -0300

    Flask-login Funcional

--- app.py
+++ app.py
@@ -1,164 +1,106 @@
-from flask import Flask , request , abort , redirect , Response ,url_for, render_template
-from flask_login import LoginManager , login_required , UserMixin , login_user, current_user
-from werkzeug.security import safe_str_cmp
+from flask import Flask
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user 
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
-from mysqldb import mysql
+
+from mysqldb1 import mysql
+# init SQLAlchemy so we can use it later in our models
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key'
 
-login_manager = LoginManager()
-login_manager.login_view = "login"
-login_manager.init_app(app)
-
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+mysqlconnector://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % mysql
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
 
-class Book(db.Model):
-    __tablename__ = 'books'
-    title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+db = SQLAlchemy()
+db.init_app(app)
 
-    def __repr__(self):
-        return "<Title: {}>".format(self.title)
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-class UserModel(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
-    password = db.Column(db.String(80))
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @classmethod
-    def find_by_username(cls, username):
-        return cls.query.filter_by(username=username).first()
-
-    @classmethod
-    def find_by_id(cls, _id):
-        return cls.query.filter_by(id=_id).first()
-
-def authenticate(username, password):
-    user = UserModel.find_by_username(username)
-    if user and safe_str_cmp(user.password, password):
-        return user
+auth = Blueprint('auth', __name__)
+app.register_blueprint(auth)
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
-@app.route('/', methods=["GET", "POST"])
-def do_login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = authenticate(username, password)
-        if user:
-            return redirect(url_for('home'))
-        else:
-            return abort(401)
-    return render_template("login.html")
+class User(UserMixin,db.Model):
+    # ...
+    __tablename__ = 'users'
 
-@login_required
-@app.route('/home', methods=["GET", "POST"])
-def home():
-    books = None
-    if request.form:
-        try:
-            book = Book(title=request.form.get("title"))
-            db.session.add(book)
-            db.session.commit()
-        except Exception as e:
-            print("Failed to add book")
-            print(e)
-    books = Book.query.all()
-    return render_template("home.html", books=books)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(128))
+    password = db.Column(db.String(128))
 
-@login_required
-@app.route("/update", methods=["POST"])
-def update():
-    try:
-        newtitle = request.form.get("newtitle")
-        oldtitle = request.form.get("oldtitle")
-        book = Book.query.filter_by(title=oldtitle).first()
-        book.title = newtitle
-        db.session.commit()
-    except Exception as e:
-        print("Couldn't update book title")
-        print(e)
-    return redirect("/home")
 
-@login_required
-@app.route("/delete", methods=["POST"])
-def delete():
-    title = request.form.get("title")
-    book = Book.query.filter_by(title=title).first()
-    db.session.delete(book)
+@app.route('/login', methods=['POST', 'GET'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+    
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return render_template('login.html')
+
+    if email and password:
+        login_user(user, remember=remember)
+        return render_template('profile.html')
+    return render_template('login.html')
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup_post():
+    email = request.form.get('email')
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+
+    if email and user: # if a user is found, we want to redirect back to signup page so user can try again
+        flash('Email address already exists')
+        return render_template('signup.html')
+
+    new_user = User(email=email, username=username, password=generate_password_hash(password, method='sha256'))
+
+    db.session.add(new_user)
     db.session.commit()
-    return redirect("/home")
 
+    return render_template('signup.html')
 
-@app.route('/login' , methods=['GET' , 'POST'])
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    #return redirect(url_for('.index'))
+    return render_template('index.html')
+
+@app.route('/login')
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = authenticate(username, password)
-        if user:
-            return redirect(url_for('home'))
-        else:
-            return abort(401)
-    else:
-        return render_template('lgn.html')
-"""        return Response('''
-            <form action="" method="post">
-                <p><input type=text name=username>
-                <p><input type=password name=password>
-                <p><input type=submit value=Login>
-            </form>
-        ''')"""
+    return render_template('login.html')
 
-@app.route('/register' , methods = ['GET' , 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
-        if UserModel.find_by_username(username):
-            return Response('''
-                <h3> Este usuário já existe</h3>
-                <a href="/register">Faça cadastro com outro username</a> ''')
-        user = UserModel(username, password)
-        user.save_to_db()
-        return Response('''
-        <h3> Você foi cadastrado com Sucesso!!!</h3>
-        <a href="/login">Faça Login Aqui</a>
-        ''')
-    else:
-        return render_template('register.html')
-        """return Response('''
-                                    <form action="" method="post">
-                                    <p><input type=text name=username placeholder="Enter username">
-                                    <p><input type=password name=password placeholder="Enter password">
-                                    <p><input type=submit value=Register>
-                                    </form>
-                                ''')"""
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# handle login failed
-@app.errorhandler(401)
-def page_not_found(e):
-    return Response('<p>Login failed</p>')
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.username)
 
-# callback to reload the user object        
 @login_manager.user_loader
-def load_user(userid):
-    return UserModel.find_by_id(userid)
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 if __name__ == '__main__':
     app.run(debug =True)
